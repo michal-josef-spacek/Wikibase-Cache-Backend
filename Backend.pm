01c61da7950d97280da4ca20082b5e1abc9c0304
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-25 10:18:23 +0200

    Prepare for 0.05 version

--- Backend.pm
+++ Backend.pm
@@ -10,7 +10,7 @@ use Readonly;
 
 Readonly::Array our @TYPES => qw(description label);
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 sub new {
 	my ($class, @params) = @_;
@@ -215,6 +215,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
